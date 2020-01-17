d011ec6909cb93ee878a4a4d2db123724cf6105f
Евгений Соколов <sokolov99workemail@gmail.com>
2020-01-17 17:23:37 +0200

    moved App.vue, added Header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Header extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    width: 100%;
+    height: 121px;
+    background: center / cover no-repeat url('../assets/images/header/top.jpg');
+  }
+</style>
