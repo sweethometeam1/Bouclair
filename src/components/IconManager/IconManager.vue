a4731ab2fa75b0661ff1c696bea9acfcf84ceebb
Евгений Соколов <sokolov99workemail@gmail.com>
2020-01-24 12:19:28 +0200

    added X and updated WindowType in IconManager

--- src/components/IconManager/IconManager.vue
+++ src/components/IconManager/IconManager.vue
@@ -3,6 +3,7 @@
   <Roller v-else-if="name === 'roller'" :width="width" :height="height" :fill="fill"></Roller>
   <ChevronRight v-else-if="name === 'chevron-right'" :width="width" :height="height" :fill="fill"></ChevronRight>
   <ChevronLeft v-else-if="name === 'chevron-left'" :width="width" :height="height" :fill="fill"></ChevronLeft>
+  <X v-else-if="name === 'x'" :width="width" :height="height" :fill="fill"></X>
 </template>
 
 <script lang="ts">
@@ -11,13 +12,15 @@ import WindowType from './components/WindowType.vue'
 import Roller from './components/Roller.vue'
 import ChevronRight from './components/ChevronRight.vue'
 import ChevronLeft from './components/ChevronLeft.vue'
+import X from './components/X.vue'
 
 @Component({
   components: {
     WindowType,
     Roller,
     ChevronRight,
-    ChevronLeft
+    ChevronLeft,
+    X
   }
 })
 export default class IconManager extends Vue {
---
+++ src/components/IconManager/components/X.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" :fill="fill" viewBox="0 0 10 10"><g><g><path d="M5.728 5.007L9.838.915a.496.496 0 0 0 0-.706.506.506 0 0 0-.713 0L5.02 4.297.875.147a.503.503 0 0 0-.713 0 .507.507 0 0 0 0 .716L4.304 5.01.147 9.147a.497.497 0 0 0 0 .707.506.506 0 0 0 .713 0l4.152-4.135L9.14 9.852a.503.503 0 0 0 .713 0 .507.507 0 0 0 0-.715z"/></g></g></svg>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class X extends Vue {
+  @Prop({ default: 10 }) private width!: string | number;
+  @Prop({ default: 10 }) private height!: string | number;
+  @Prop() private fill?: string | number;
+}
+</script>
