c5a45f309fd449f67c8bedb60617aff9b03f4731
Евгений Соколов <sokolov99workemail@gmail.com>
2020-01-28 11:51:18 +0200

    added adaptivity to container.vue

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -14,9 +14,34 @@ export default class Container extends Vue {}
 <style lang="scss" scoped>
   .container {
     max-width: 1170px;
+    width: 100%;
     display: flex;
     justify-content: space-between;
     margin-left: auto;
     margin-right: auto;
+  }
+
+  @media screen and (max-width: 1200px) {
+    .container {
+      padding: 0 15px;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .container {
+      padding: 0 70px;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    .container {
+      padding: 0 15px;
+    }
+  }
+
+  @media screen and (min-width: 640px) and (max-width: 768px) and (orientation: landscape) {
+    .container {
+      padding: 0 15px;
+    }
   }
 </style>
